d5a4570a07cfb026fd8b1afd5d0910908a8a7878
Priyanka Kumari <64683009+decodepriyanka@users.noreply.github.com>
2022-10-21 00:23:34 +0530

    Update game.js

--- game.js
+++ game.js
@@ -56,7 +56,7 @@ function nextSequence() {
 
   // $("#" + randomChosenColour).on("click", function () {
   $("#" + randomChosenColour)
-    .fadeIn(100)
+    .fadeIn(101)
     .fadeOut(100)
     .fadeIn(100);
   playSound(randomChosenColour);
